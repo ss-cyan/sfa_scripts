1a66ec3a0aadb316287b09d5043e9016386f7363
ss-cyan <sscynthiayan@gmail.com>
2020-10-14 19:21:52 -0500

    5th commit. Increment save.

--- src/sceneFile.py
+++ src/sceneFile.py
@@ -54,3 +54,33 @@ class SceneFile(object):
                         "directories...")
             self.folder_path.makedirs_p()
             return pmc.system.saveAs(self.path)
+
+    def next_avail_ver(self):
+        """Returns the next available version number in the folder."""
+        pattern = "{descriptor}_{task}_v{ext}".format(
+            descriptor=self.descriptor, task=self.task, ext=self.ext)
+        matching_scenefiles = []
+        for file_ in self.folder_path.files():
+            if file_.name.fnmatch(pattern):
+                matching_scenefiles.append(file_)
+        if not matching_scenefiles:
+            return 1
+        matching_scenefiles.sort(reverse=True)
+        latest_scenefile = matching_scenefiles[0]
+        latest_scenefile = latest_scenefile.name.stripext()
+        latest_ver_num = int(latest_scenefile.split("v")[-1])
+        return latest_ver_num + 1
+
+
+    def increment_save(self):
+        """Increments the version and saves the scene file.
+
+        If the existing version of a file already exists, it should
+        increment from the largest version number available in the
+        folder.
+
+        Returns:
+            Path: The path the scene file if successful.
+        """
+        self.ver = self.next_avail_ver()
+        self.save()
